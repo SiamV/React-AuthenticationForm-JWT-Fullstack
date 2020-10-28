3a6ea4d3b08307d42fc01f45dd96453a506e551c
Vitaly <avers3452@gmail.com>
2020-10-28 11:54:45 -0500

    thaiwind css login form

--- client/components/Login/Login.js
+++ client/components/Login/Login.js
@@ -3,17 +3,18 @@ import '../../stylesTailwind/main.css'
 
 const Login = () => {
     return (
-        <div className="w-full max-w-xs">
-            <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
-                <div className="mb-4">
-                    <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="username">
-                        Username
+        <div className="flex justify-center items-center ">
+            <div className="w-full max-w-xs ">
+                <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+                    <div className="mb-4">
+                        <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="username">
+                            Username
                     </label>
-                    <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                        id="username"
-                        type="text"
-                        placeholder="Username" />
-                </div>
+                        <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                            id="username"
+                            type="text"
+                            placeholder="Username" />
+                    </div>
                     <div className="mb-6">
                         <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">
                             Password
@@ -22,14 +23,15 @@ const Login = () => {
                             id="password"
                             type="password"
                             placeholder="******************" />
-                            <p className="text-red-500 text-xs italic">Please choose a password.</p>
+                        <p className="text-red-500 text-xs italic">Please choose a password.</p>
                     </div>
-                        <div className="flex items-center justify-between">
-                            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
-                                Sign In
+                    <div className="flex items-center justify-between">
+                        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
+                            Sign In
                             </button>
-                        </div>
-            </form>
+                    </div>
+                </form>
+            </div>
         </div>
     )
 }
