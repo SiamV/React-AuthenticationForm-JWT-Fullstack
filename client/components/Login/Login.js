2d48dbf8e0a155c393ff7f40b3a3b5f62207bf3d
Vitaly <avers3452@gmail.com>
2020-10-28 02:09:16 -0500

    addd tailwind and login form

--- client/components/Login/Login.js
+++ client/components/Login/Login.js
@@ -1,9 +1,35 @@
 import React from 'react';
+import '../../stylesTailwind/main.css'
 
 const Login = () => {
     return (
-        <div>
-            Here will be Login component.
+        <div className="w-full max-w-xs">
+            <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+                <div className="mb-4">
+                    <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="username">
+                        Username
+                    </label>
+                    <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                        id="username"
+                        type="text"
+                        placeholder="Username" />
+                </div>
+                    <div className="mb-6">
+                        <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">
+                            Password
+                        </label>
+                        <input className="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+                            id="password"
+                            type="password"
+                            placeholder="******************" />
+                            <p className="text-red-500 text-xs italic">Please choose a password.</p>
+                    </div>
+                        <div className="flex items-center justify-between">
+                            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
+                                Sign In
+                            </button>
+                        </div>
+            </form>
         </div>
     )
 }
