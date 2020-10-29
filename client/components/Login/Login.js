67fe0935319da7d36562366d74fba7875f927852
Vitaly <avers3452@gmail.com>
2020-10-29 11:38:49 -0500

    connect REDUX_DEVTOOLS in redux store.js

--- client/components/Login/Login.js
+++ client/components/Login/Login.js
@@ -31,17 +31,17 @@ const Login = () => {
                         <input className="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
                             id="password"
                             type="password"
-                            placeholder="******************" 
+                            placeholder="******************"
                             value={password}
-                            onChange = {(e) => {
-                            dispatch(setPasswordFieldAC(e.target.value)) 
-                        }} />
+                            onChange={(e) => {
+                                dispatch(setPasswordFieldAC(e.target.value))
+                            }} />
                         <p className="text-red-500 text-xs italic">Please choose a password.</p>
                     </div>
                     <div className="flex items-center justify-between">
                         <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
                             Sign In
-                            </button>
+                        </button>
                     </div>
                 </form>
             </div>
--- client/redux/store.js
+++ client/redux/store.js
@@ -1,4 +1,4 @@
-import {applyMiddleware, combineReducers, createStore} from "redux";
+import {applyMiddleware, combineReducers, createStore, compose} from "redux";
 import thunkMiddleware from "redux-thunk";
 import loginReducer from "./reducers/loginReducer";
 
@@ -7,6 +7,12 @@ let reducersStack = combineReducers({
     login: loginReducer,
 })
 
-let store = createStore(reducersStack, applyMiddleware(thunkMiddleware));
+//connect redux chrome extension. delete after develop
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+let store = createStore(reducersStack, composeEnhancers(applyMiddleware(thunkMiddleware)));
 
 export default store;
+
+// store without redux chrome extension
+// let store = createStore(reducersStack, applyMiddleware(thunkMiddleware));
